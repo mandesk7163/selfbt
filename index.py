5eb5e8d427d0b2cf4edaaedc36cf7a73d0a0b8c2
mandesk7163 <mandesk132@gmail.com>
2025-04-13 15:53:13 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,29 +1,19 @@
-import discord
-from discord.ext import commands
+from selfcord import Client
 from dotenv import load_dotenv
 import os
 
-# Carregar as variáveis do arquivo .env
 load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
 
-# Obter o token do Discord a partir do arquivo .env
-token = os.getenv('DISCORD_TOKEN')
-
-# Criar uma instância de Intents
-intents = discord.Intents.default()
-intents.message_content = True  # Permitir que o bot leia o conteúdo das mensagens
-
-# Criação do client Discord com intents
-client = discord.Client(intents=intents)
+client = Client()
 
 @client.event
 async def on_ready():
-    print(f'Logado como {client.user}')
+    print(f"Logado como {client.user}")
 
 @client.event
 async def on_message(message):
-    if message.content == "!ping":
+    if message.author == client.user and message.content == "!ping":
         await message.channel.send("Pong!")
 
-# Rodar o bot (self bot) com bot=False
 client.run(token, bot=False)
