f571a065d4d095a5fefe9ff298e2e7797e23f612
mandesk7163 <mandesk132@gmail.com>
2025-04-13 15:47:45 -0300

    Update index.py

--- index.py
+++ index.py
@@ -9,8 +9,12 @@ load_dotenv()
 # Obter o token do Discord a partir do arquivo .env
 token = os.getenv('DISCORD_TOKEN')
 
-# Criação do cliente Discord
-client = discord.Client()
+# Criar uma instância de Intents
+intents = discord.Intents.default()
+intents.message_content = True  # Permitir que o bot leia o conteúdo das mensagens
+
+# Criação do client Discord com intents
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
